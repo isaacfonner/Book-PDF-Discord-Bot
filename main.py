c6b6d9c9248b8f1e78d1599bec407c91d1138f03
isaacfonner <61657908+isaacfonner@users.noreply.github.com>
2024-06-02 21:45:21 +0000

    W

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import os
 from libgen_api import LibgenSearch
 import discord
 import json
+from discord import app_commands
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="/", intents=intents)
 
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+  await client.tree.sync()
+  print('Synced slash commands with Discord')
+  await client.change_presence(activity=discord.Activity(
+    type=discord.ActivityType.listening, name="/book"))
+  print('------')
 
 
 @client.event
@@ -22,19 +29,25 @@ async def on_message(message):
     return
   if message.content.startswith('$book'):
     await message.channel.send("PDF will now be sent. if none get sent, no book was found")
-
-  if message.content.startswith('$book'):
+@client.tree.command(name="book", description = "Get a PDF of a certain book")
+@app_commands.describe(book="The book title to search")
+async def book(i: discord.Interaction, book: str):
+   # await i.response.send_message("PDF will now be sent. if none get sent, no book was found")
     s = LibgenSearch()
-    results = s.search_title(message.content)
-    print(results)
+    results = s.search_title(book)
     item_to_download = results[0]
     download_links = s.resolve_download_links(item_to_download)
     y = json.dumps(download_links)
     where = y.find('"Cloudflare":')
     where2 = y.find('"IPFS.io":')
     burger = len(y) - where2
-    await message.channel.send(y[where+15:burger*-1])
-
+    embed = discord.Embed(title="Book Found", description=y[where+15:burger*-1])
+    await i.response.send_message(embed=embed)
+@client.tree.error
+async def on_app_command_error(i: discord.Interaction, error):
+  if isinstance(error, app_commands.errors.CommandInvokeError):
+    embed = discord.Embed(title="Error", description="No book was found for the search term you provided.")
+    await i.response.send_message(embed=embed)  
 try:
   token = os.getenv("TOKEN") or ""
   if token == "":
